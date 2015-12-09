0a7279ed461904c61ababbd5108ad6088083c6ca
Yuchen Wang <yulon@me.com>
2015-12-09 03:51:37 +0100

    Remove

--- enums.go
+++
@@ -1,8 +0,0 @@
-package main
-
-// An Enum represents an enumerated value.
-type Enum struct {
-	Name   string // Raw specification name
-	GoName string // Go name with the API prefix stripped
-	Value  string // Raw specification value
-}
